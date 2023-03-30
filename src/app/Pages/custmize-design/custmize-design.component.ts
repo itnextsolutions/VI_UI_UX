9758056f5b8efc45fb729acb2b701840204f37d2
itnextsolutions <itnextsolutions.web@gmail.com>
2023-03-30 13:19:53 +0530

    product details changes by vishal

--- src/app/Pages/custmize-design/custmize-design.component.ts
+++ src/app/Pages/custmize-design/custmize-design.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { UserService } from 'src/app/Services/Api/User/user.service';
 
 @Component({
   selector: 'app-custmize-design',
@@ -6,27 +7,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./custmize-design.component.css']
 })
 export class CustmizeDesignComponent implements OnInit {
+  @Input() Product: any;
+  @Input() id:number=0; 
+  frontimage=false;
+  sideimage = true;
+  backimage = true;
 
-  modalTitle:any;
-  activateCustmizeDesign:boolean = false;
-  CustmizeDesign:any;
+  modalTitle: any;
+  activateCustmizeDesign: boolean = false;
+  CustmizeDesign: any;
+
+  frontImage: any = "";
+  backImage: any = "";
+  sideImage: any = "";
+  catName: any = "";
+  image:any="";
 
   imageSrc = '';
   messageText = '';
   imageButtons = [ 
   {src:'../assets/img/Shirts/pro-1-1.jpeg', name: 'Front'}, 
   {src:'../assets/img/Shirts/pro-1-3.jpeg', name: 'Back'}, 
-  {src:'../assets/img/Shirts/pro-1-2.jpeg', name: 'Side'},]
+  {src:'../assets/img/Shirts/pro-1-2.jpeg', name: 'Side'}]
+
+ 
 
 
-  
-  constructor() { }
+
+  constructor(private userService: UserService) { }
   ngOnInit(): void {
-    this.imageSrc='../assets/img/Shirts/pro-1-1.jpeg';
+    // this.imageSrc = this.frontImage;
   }
-  onClick(imageNameObject : any) {
-    this.imageSrc = imageNameObject.src;
-    this.messageText = imageNameObject.name;
+  // onClick(imageNameObject: any) {
+  //   this.imageSrc = imageNameObject.src;
+    
+  //   this.messageText = imageNameObject.name;
+  // }
+
+  onClickFrontPhoto() {
+  
+    // this.userService.getProductById(this.productId).subscribe(data => {
+    //   this.product = data;
+    // });
+
+  this.sideimage = true;
+  this.backimage = true;
+  this.frontimage = false;
+}
+
+  onClickSidetPhoto() {
+    
+    // this.userService.getProductById(this.productId).subscribe(data => {
+    //   this.product = data;
+    // });
+
+  this.sideimage = false;
+  this.backimage = true;
+  this.frontimage = true;
   }
 
+  onClickBackPhoto() {
+  
+    // this.userService.getProductById(this.productId).subscribe(data => {
+    //   this.product = data;
+    // });
+
+  this.sideimage = true;
+  this.backimage = false;
+  this.frontimage = true;
+}
 }
--- src/app/Pages/product-details/product-details.component.ts
+++ src/app/Pages/product-details/product-details.component.ts
@@ -1,3 +1,83 @@
+// import { Component, OnInit } from '@angular/core';
+// import { ActivatedRoute } from '@angular/router';
+// import { UserService } from 'src/app/Services/Api/User/user.service';
+// import { __values } from 'tslib';
+
+// @Component({
+//   selector: 'app-product-details',
+//   templateUrl: './product-details.component.html',
+//   styleUrls: ['./product-details.component.css']
+// })
+// export class ProductDetailsComponent implements OnInit {
+//   [x: string]: any;
+//   route: any;
+
+//   constructor(private param: ActivatedRoute, private userService: UserService) { }
+
+//   categoryfolder: any ;
+//   product: any;
+//   category_id: any;
+//   sizes: any = [];
+//   colors: any = [];
+//   simillarProducts: any = [];
+//   hidden:any;
+ 
+//   ngOnInit(): void {
+//     this.productId =this.param.snapshot.paramMap.get('id');
+//     this.categoryName =this.param.snapshot.paramMap.get('categoryName');
+//     this.categoryName = this.categoryName.replace(/-/g, ' ').toUpperCase();
+    
+//     this.subcategoryName =this.param.snapshot.paramMap.get('subcategoryName');
+//     this.subcategoryName = this.subcategoryName.replace(/-/g, ' ').toUpperCase();
+
+//     this.getProduct();
+//     this.getSimillarProduct();
+//     this.imageSource();
+//   }
+
+//   getSimillarProduct()
+//   {
+    
+//     this.userService.getSimillarProduct(this.productId).subscribe(data =>{
+//     this.simillarProducts = data;
+//      this.categoryfolder = this.categoryName.replace(/\s+/g, '-').toLowerCase();
+//     });
+//   }
+
+//   getProduct()
+//   {
+//       this.userService.getProductById(this.productId).subscribe(data =>{
+//       this.product = data;
+
+//       if (this.product != null) {
+//         this.product.forEach((element:any) => {
+//           this.category_id = element.Category_Id;
+//         })
+//       }
+//      });
+
+//      this.userService.getColorListById(this.productId).subscribe(data =>{
+//       this.colors =data;
+//      });
+
+//      this.userService.getSizeListById(this.productId).subscribe(data =>{
+//       this.sizes = data;
+
+      
+//      });
+//   }
+
+//   onClick(product: any){
+//     this.router.navigate(['product-details/', product.Category_Name, product.SubCategory, product.Product_Id]);
+
+//     // this.router.navigate(['blog/', data.Blog_Id]).then(() => {
+//     //   window.location.reload();
+//     this.ngOnInit();
+//   }
+ 
+ 
+// }
+
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from 'src/app/Services/Api/User/user.service';
@@ -14,20 +94,27 @@ export class ProductDetailsComponent implements OnInit {
 
   constructor(private param: ActivatedRoute, private userService: UserService) { }
 
-  categoryfolder: any ;
+  // isMenDivHidden = false;
+  isWoMenDivHidden = true;
+  categoryfolder: any;
   product: any;
   category_id: any;
   sizes: any = [];
   colors: any = [];
   simillarProducts: any = [];
-  hidden:any;
- 
+  hidden: any;
+
+  menDesc: string = "";
+  womenDesc: string = "";
+
+  id:number=0; 
+
   ngOnInit(): void {
-    this.productId =this.param.snapshot.paramMap.get('id');
-    this.categoryName =this.param.snapshot.paramMap.get('categoryName');
+    this.productId = this.param.snapshot.paramMap.get('id');
+    this.categoryName = this.param.snapshot.paramMap.get('categoryName');
     this.categoryName = this.categoryName.replace(/-/g, ' ').toUpperCase();
-    
-    this.subcategoryName =this.param.snapshot.paramMap.get('subcategoryName');
+
+    this.subcategoryName = this.param.snapshot.paramMap.get('subcategoryName');
     this.subcategoryName = this.subcategoryName.replace(/-/g, ' ').toUpperCase();
 
     this.getProduct();
@@ -35,47 +122,69 @@ export class ProductDetailsComponent implements OnInit {
     this.imageSource();
   }
 
-  getSimillarProduct()
-  {
-    
-    this.userService.getSimillarProduct(this.productId).subscribe(data =>{
-    this.simillarProducts = data;
-     this.categoryfolder = this.categoryName.replace(/\s+/g, '-').toLowerCase();
+  getSimillarProduct() {
+
+    this.userService.getSimillarProduct(this.productId).subscribe(data => {
+      this.simillarProducts = data;
+      this.categoryfolder = this.categoryName.replace(/\s+/g, '-').toLowerCase();
     });
   }
 
-  getProduct()
-  {
-      this.userService.getProductById(this.productId).subscribe(data =>{
+  getProduct() {
+    this.userService.getProductById(this.productId).subscribe(data => {
       this.product = data;
 
       if (this.product != null) {
-        this.product.forEach((element:any) => {
+        this.product.forEach((element: any) => {
           this.category_id = element.Category_Id;
         })
       }
-     });
+    });
 
-     this.userService.getColorListById(this.productId).subscribe(data =>{
-      this.colors =data;
-     });
+    this.userService.getColorListById(this.productId).subscribe(data => {
+      this.colors = data;
+    });
 
-     this.userService.getSizeListById(this.productId).subscribe(data =>{
+    this.userService.getSizeListById(this.productId).subscribe(data => {
       this.sizes = data;
 
-      
-     });
+
+    });
   }
 
-  onClick(product: any){
+  onClick(product: any) {
     this.router.navigate(['product-details/', product.Category_Name, product.SubCategory, product.Product_Id]);
 
     // this.router.navigate(['blog/', data.Blog_Id]).then(() => {
     //   window.location.reload();
     this.ngOnInit();
   }
- 
- 
+
+
+  onClickWomenPhoto() {
+  
+      this.userService.getProductById(this.productId).subscribe(data => {
+        this.product = data;
+      });
+
+    this.isMenDivHidden = true;
+    this.isWoMenDivHidden = false;
+    this.menDesc=this.product.Product_Description;
+    this.id=1;
+    
+  }
+
+  onClickmenPhoto() {
+    
+    this.userService.getProductById(this.productId).subscribe(data => {
+      this.product = data;
+    });
+    this.isMenDivHidden = false;
+    this.isWoMenDivHidden = true;
+
+    this.womenDesc=this.product.WomenProduct_Description;
+    this.id=0;
+  }
 }
 
 
