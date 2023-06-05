dcdbf3c33e08cc4358485e49f6fe2ecf37f87b56
itnextsolutions <itnextsolutions.web@gmail.com>
2023-06-05 12:00:19 +0530

    changes by vishal

--- src/app/Pages/product-details/product-details.component.ts
+++ src/app/Pages/product-details/product-details.component.ts
@@ -13,12 +13,12 @@ export class ProductDetailsComponent implements OnInit {
   [x: string]: any;
   route: any;
 
-  constructor(private param: ActivatedRoute, private userService: UserService,private router: Router, private seoService: SeoService) {}
+  constructor(private param: ActivatedRoute, private userService: UserService, private router: Router, private seoService: SeoService) { }
 
   isMenDivHidden = false;
   isWoMenDivHidden = true;
-  isTippingDivHidden=true;
-  isTippingWomenDivHidden=true;
+  isTippingDivHidden = true;
+  isTippingWomenDivHidden = true;
 
   categoryfolder: any;
   product: any;
@@ -33,14 +33,15 @@ export class ProductDetailsComponent implements OnInit {
   //  menDesc: string = "";
   //  womenDesc: string = "";
   // tipping:any=[];
-  id:number=0; 
+  id: number = 0;
 
-  slideConfig = {dots: true,
+  slideConfig = {
+    dots: true,
     speed: 300,
     slidesToShow: 6,
     slidesToScroll: 6,
     infinite: false,
-    // column:6,
+    column:6,
     // rows:3,
     responsive: [
       {
@@ -50,7 +51,7 @@ export class ProductDetailsComponent implements OnInit {
           slidesToScroll: 3,
           infinite: true,
           dots: true,
-          rows:3,
+          rows: 3,
         }
       },
       {
@@ -58,7 +59,7 @@ export class ProductDetailsComponent implements OnInit {
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
-          rows:3,
+          rows: 3,
         }
       },
 
@@ -67,19 +68,19 @@ export class ProductDetailsComponent implements OnInit {
         settings: {
           slidesToShow: 4,
           slidesToScroll: 4,
-          rows:3,
+          rows: 3,
         }
       },
       {
         breakpoint: 380,
         settings: {
-          slidesToShow:3,
+          slidesToShow: 3,
           slidesToScroll: 3,
-          rows:3,
+          rows: 3,
         }
       }
     ]
-}
+  }
 
   ngOnInit(): void {
     this.productId = this.param.snapshot.paramMap.get('id');
@@ -91,7 +92,7 @@ export class ProductDetailsComponent implements OnInit {
     this.subcategoryName = this.param.snapshot.paramMap.get('subcategoryName');
     this.subcategoryName = this.subcategoryName.replace(/-/g, ' ').toUpperCase();
 
-    this.isbrand=this.param.snapshot.paramMap.get('isbrand');
+    this.isbrand = this.param.snapshot.paramMap.get('isbrand');
 
     this.getProduct();
     this.getSimillarProduct();
@@ -122,11 +123,17 @@ export class ProductDetailsComponent implements OnInit {
 
     this.userService.getColorListById(this.productId).subscribe(data => {
       this.colors = data;
-// this.color_count=this.colors.length;
-// if(this.color_count>=6)
-// {
-
-// }
+      // this.color_count = this.colors.length;
+      // if (this.color_count >=18) {
+      //   if(this.color_count.slice(0,6))
+      //   {
+      //     this.slideConfig.rows=1;
+      //   }
+      //   if(this.color_count.slice(7,12))
+      //   {
+      //     this.slideConfig.rows=2;
+      //   }
+      // }
 
     });
 
@@ -134,14 +141,14 @@ export class ProductDetailsComponent implements OnInit {
       this.sizes = data;
     });
 
-    this.userService.getTipingListById(this.productId).subscribe(data=>{
-      this.tipping=data; 
-   });
+    this.userService.getTipingListById(this.productId).subscribe(data => {
+      this.tipping = data;
+    });
 
 
-   this.userService.getTipingWomenListById(this.productId).subscribe(data=>{
-    this.tippingWomen=data; 
- });
+    this.userService.getTipingWomenListById(this.productId).subscribe(data => {
+      this.tippingWomen = data;
+    });
   }
 
   onClick(product: any) {
@@ -156,53 +163,53 @@ export class ProductDetailsComponent implements OnInit {
 
 
   onClickWomenPhoto() {
-  
-      this.userService.getProductById(this.productId).subscribe(data => {
-        this.product = data;
-      });
-      this.productcategoryfolder = this.categoryName.replace(/\s+/g, '-').toLowerCase();
+
+    this.userService.getProductById(this.productId).subscribe(data => {
+      this.product = data;
+    });
+    this.productcategoryfolder = this.categoryName.replace(/\s+/g, '-').toLowerCase();
     this.isMenDivHidden = true;
     this.isWoMenDivHidden = false;
-    this.isTippingDivHidden=true;
-    this.isTippingWomenDivHidden=true;
+    this.isTippingDivHidden = true;
+    this.isTippingWomenDivHidden = true;
     // this.menDesc=this.product.Product_Description;
-    this.id=1;
-    
+    this.id = 1;
+
   }
 
   onClickmenPhoto() {
-    
+
     this.userService.getProductById(this.productId).subscribe(data => {
       this.product = data;
     });
     this.productcategoryfolder = this.categoryName.replace(/\s+/g, '-').toLowerCase();
     this.isMenDivHidden = false;
     this.isWoMenDivHidden = true;
-    this.isTippingDivHidden=true;
-    this.isTippingWomenDivHidden=true;
+    this.isTippingDivHidden = true;
+    this.isTippingWomenDivHidden = true;
 
     // this.womenDesc=this.product.WomenProduct_Description;
-    this.id=0;
+    this.id = 0;
   }
 
-  onTippingClick(item:any){
+  onTippingClick(item: any) {
     // this.userService.getTipingListById(this.productId).subscribe(data=>{
     //   this.tipping=data; 
-    
+
     // });
     // this.tipping.filter((x: { tipping_Code: number; })=>x.tipping_Code==code);
     this.productcategoryfolder = this.categoryName.replace(/\s+/g, '-').toLowerCase();
     this.isMenDivHidden = true;
     this.isWoMenDivHidden = true;
-    this.isTippingDivHidden=false;
-    this.isTippingWomenDivHidden=true;
-    this.image=item.tipping_big_img;
-    this.id=2;
+    this.isTippingDivHidden = false;
+    this.isTippingWomenDivHidden = true;
+    this.image = item.tipping_big_img;
+    this.id = 2;
     // (this.item.filter((x: { tipping_Code: number; })=>{return x.tipping_Code==item;}));
 
   }
 
-  onTippingWomenClick(item:any){
+  onTippingWomenClick(item: any) {
 
     // this.isMenDivHidden = true;
     // this.isWoMenDivHidden = true;
@@ -214,21 +221,20 @@ export class ProductDetailsComponent implements OnInit {
 
     this.isMenDivHidden = true;
     this.isWoMenDivHidden = true;
-    this.isTippingDivHidden=true;
-    this.isTippingWomenDivHidden=false;
-    this.image1=item.tipping_big_img;
-    this.id=3;
+    this.isTippingDivHidden = true;
+    this.isTippingWomenDivHidden = false;
+    this.image1 = item.tipping_big_img;
+    this.id = 3;
     this.productcategoryfolder = this.categoryName.replace(/\s+/g, '-').toLowerCase();
-    
+
   }
 
-  onColorClick(data:any) {
-    if(data.Description!=null)
-    {
+  onColorClick(data: any) {
+    if (data.Description != null) {
       this.bgColor = data.Description;
       this.selected_colorname = data.ColorName;
     }
-    else{
+    else {
       this.bgColor = '#FFFFFF';
     }
   }
